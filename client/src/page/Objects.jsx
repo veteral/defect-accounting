c682225393797e513bf72167802bd692a61858bc
veteral <alexzpv@yandex.ru>
2021-03-06 10:48:41 +0300

    changed Objects

--- client/src/page/Objects.jsx
+++ client/src/page/Objects.jsx
@@ -1,7 +1,42 @@
+import { Table } from 'antd';
+
+
 const Objects = () => {
+    const dataSource = [
+        {
+          key: '1',
+          name: 'Mike',
+          age: 32,
+          address: '10 Downing Street',
+        },
+        {
+          key: '2',
+          name: 'John',
+          age: 42,
+          address: '10 Downing Street',
+        },
+      ];
+      
+      const columns = [
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'Age',
+          dataIndex: 'age',
+          key: 'age',
+        },
+        {
+          title: 'Address',
+          dataIndex: 'address',
+          key: 'address',
+        },
+      ];
     return (
         <>
-            objects
+            <Table dataSource={dataSource} columns={columns} />;
         </>
     )
 }
