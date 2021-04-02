dba44dbb4fa0b9ca0a11b069d48cb9b18532e136
veteral <alexzpv@yandex.ru>
2021-04-02 18:38:37 +0400

    added page ControlReport

--- client/src/page/reports/ControlReport.jsx
+++ client/src/page/reports/ControlReport.jsx
@@ -1,13 +1,84 @@
-import { DatePicker, Button } from "antd";
-
+import { Form, Input, Button, DatePicker, TimePicker } from "antd";
+const layout = {
+    labelCol: {
+        span: 8,
+    },
+    wrapperCol: {
+        span: 16,
+    },
+};
+const tailLayout = {
+    wrapperCol: {
+        offset: 5,
+        span: 16,
+    },
+};
+const tailLayout1 = {
+    wrapperCol: {
+        offset: 8,
+        span: 16,
+    },
+};
 export const ControlReport = () => {
+    const onFinish = (values) => {
+        console.log("Success:", values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log("Failed:", errorInfo);
+    };
+
     return (
         <div className="reports">
-            <h1>Контроль за сработавшими объектами</h1>
-            <DatePicker />
-            <DatePicker />
-            <Button type="primary">Отмена</Button>
-            <Button type="primary">Сформировать</Button>
+            <h1>Контроль за сработавшими объектами за период:</h1>
+            <Form
+                // {...layout}
+                name="basic"
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+            >
+                <Form.Item {...tailLayout}>
+                    <Form.Item
+                        name="date"
+                        rules={[{ required: true, message: "Выберите дату!" }]}
+                        style={{
+                            display: "inline-block",
+                            paddingRight: "30px",
+                        }}
+                    >
+                        <DatePicker />
+                    </Form.Item>
+                    <Form.Item
+                        name="time"
+                        rules={[{ required: true, message: "Выберите время!" }]}
+                        style={{ display: "inline-block" }}
+                    >
+                        <DatePicker />
+                    </Form.Item>
+                </Form.Item>
+
+                <Form.Item {...tailLayout1} style={{ marginBottom: 0 }}>
+                    <Form.Item
+                        name="print"
+                        style={{
+                            display: "inline-block",
+                            paddingRight: "10px",
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Печать
+                        </Button>
+                    </Form.Item>
+                    <Form.Item
+                        name="cancel"
+                        style={{ display: "inline-block" }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Отмена
+                        </Button>
+                    </Form.Item>
+                </Form.Item>
+            </Form>
         </div>
     );
 };
